d04af2a72a6aa09aa5347c9ca172f13fdf3b4af6
Mohd Shaquib Khan <mohammadshaquib4@gmail.com>
2021-02-02 12:08:59 +0530

    hjk

--- app/src/main/java/com/example/localzeadmin/ContinueIn.kt
+++ app/src/main/java/com/example/localzeadmin/ContinueIn.kt
@@ -2,10 +2,24 @@ package com.example.localzeadmin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ContinueIn : AppCompatActivity() {
+    private lateinit var btnCustomer:Button
+    private lateinit var btnSeller:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_continue_in)
+
+        btnCustomer=findViewById(R.id.btnCustomer)
+        btnSeller=findViewById(R.id.btnseller)
+
+        btnCustomer.setOnClickListener {
+
+        }
+
+        btnSeller.setOnClickListener {
+
+        }
     }
 }
