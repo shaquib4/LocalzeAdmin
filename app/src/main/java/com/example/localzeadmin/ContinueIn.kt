9e1b2ff96067ef4e724f265ebf1db6b7a5e07c9c
Mohd Shaquib Khan <mohammadshaquib4@gmail.com>
2021-02-02 11:47:20 +0530

    gh

---
+++ app/src/main/java/com/example/localzeadmin/ContinueIn.kt
@@ -0,0 +1,11 @@
+package com.example.localzeadmin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContinueIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_continue_in)
+    }
+}
