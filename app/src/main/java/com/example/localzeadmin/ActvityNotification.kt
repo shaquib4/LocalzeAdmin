37d67e2ad75bc6ec9d0d7c400ae29411a665947f
Mohd Shaquib Khan <mohammadshaquib4@gmail.com>
2021-02-07 20:09:24 +0530

    hjk

---
+++ app/src/main/java/com/example/localzeadmin/ActvityNotification.kt
@@ -0,0 +1,11 @@
+package com.example.localzeadmin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActvityNotification : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actvity_notification)
+    }
+}
