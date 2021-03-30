19bf255abb94060e2e8f8ec1e507c3aeb7ca19ac
Ekagra Agrawal <ekagraagrawal49@gmail.com>
2021-03-30 22:10:57 +0100

    bank details activity

---
+++ app/src/main/java/com/example/localzeadmin/BankDetails.kt
@@ -0,0 +1,11 @@
+package com.example.localzeadmin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BankDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bank_details)
+    }
+}
