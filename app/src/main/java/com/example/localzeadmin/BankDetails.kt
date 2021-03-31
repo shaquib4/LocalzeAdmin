7855a9da477fbc04dcb498e4c1e39aa714bd38d2
Ekagra Agrawal <ekagraagrawal49@gmail.com>
2021-03-31 22:04:58 +0100

    change bank details

--- app/src/main/java/com/example/localzeadmin/BankDetails.kt
+++ app/src/main/java/com/example/localzeadmin/BankDetails.kt
@@ -2,6 +2,8 @@ package com.example.localzeadmin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -51,6 +53,52 @@ class BankDetails : AppCompatActivity() {
                 }
             }
 
+        })
+        editSearch.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                searchSeller(s.toString())
+            }
+
+        })
+    }
+
+    private fun searchSeller(s: String) {
+        val querySellerBank =
+            FirebaseDatabase.getInstance().reference.child("seller").orderByChild("phone")
+                .startAt(s).endAt(s + "\uf8ff")
+        querySellerBank.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                (listSellersDetails as ArrayList<ModalSellerInfo>).clear()
+                for (i in snapshot.children) {
+                    if (i.child("Account_Details").value.toString() == "true" && !i.child("razorpayId")
+                            .exists()
+                    ) {
+                        val shopId = i.child("shopId").value.toString()
+                        val shopName = i.child("shop_name").value.toString()
+                        val sellerName = i.child("name").value.toString()
+                        val phoneNumber = i.child("phone").value.toString()
+                        val shopAddress = i.child("locality").value.toString()
+                        val obj =
+                            ModalSellerInfo(shopId, shopName, sellerName, phoneNumber, shopAddress)
+                        (listSellersDetails as ArrayList<ModalSellerInfo>).add(obj)
+                        adapterBankDetails =
+                            AdapterSellerInfo(this@BankDetails, listSellersDetails, "BankDetails")
+                        recyclerSellersDetails.adapter = adapterBankDetails
+                    }
+                }
+            }
         })
     }
 }
